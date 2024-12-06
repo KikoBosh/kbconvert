99995fe4c18603445d6d3f854763bcca1c61b42c
KikoBosh <kristijanbosnakovski4@gmail.com>
2024-12-06 22:25:01 +0100

    fix

--- app/api/convert/route.ts
+++ app/api/convert/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import sharp from 'sharp';
 import pngToIco from 'png-to-ico';
 
+export const runtime = 'edge';
+
 export async function POST(req: NextRequest) {
   try {
     const formData = await req.formData();
